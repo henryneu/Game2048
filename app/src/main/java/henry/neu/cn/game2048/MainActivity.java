d22b17542f4e06126e39671f8123b44dbe094368
henryneu <neuyimi@gmail.com>
2017-02-28 21:52:18 +0800

    搭建完成游戏主界面

---
+++ app/src/main/java/henry/neu/cn/game2048/MainActivity.java
@@ -0,0 +1,13 @@
+package henry.neu.cn.game2048;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
